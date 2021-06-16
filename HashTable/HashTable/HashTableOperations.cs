f6fcb53eccfd02e8f3e02f7add9466db575c1346
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-17 00:12:03 +0530

    Added an method for insert

---
+++ HashTable/HashTable/HashTableOperations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+
+    internal class Node<T>
+    {
+        int key;
+        int value;
+        public Node<int> next;
+        private int v;
+
+        Node(int key, int values)
+        {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(int v)
+        {
+            this.v = v;
+        }
+        public void insert()
+        {
+            Node<int> node = new Node<int>(key);
+
+        }
+    }
+    class HashTableOperations
+    {
+        Node<int>[] keyarray = new Node<int>[11];
+        public void HashFunction()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Node<int> node = new Node<int>(-1);
+                keyarray[i] = node;
+            }
+
+        }
+        public void insert(int key)
+        {
+            Node<int> node = new Node<int>(key);
+            Node<int> temp;
+
+            int index = key % 11;
+            temp = keyarray[index];
+
+            if (temp is null)
+            {
+                temp = node;
+            }
+            else
+            {
+                while (temp.next!= null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+
+        }//end insert
+
+       
+
+    }
+
+}
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HashTableOperations objHash = new HashTableOperations();
+            objHash.HashFunction();
+            objHash.insert(1);
+        }
+    }
+}
