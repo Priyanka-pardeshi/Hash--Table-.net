14de7ecccf141379110840cbcb16937f7d6d5c8a
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-17 11:19:19 +0530

    Write a method for get value

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -15,16 +15,17 @@ namespace HashTable
             string varGetValue = objHash.getValue(3);
             Console.WriteLine(" Value of an incex is =" + varGetValue);
 
-            HashMap<int, string> strObjectHash = new HashMap<int, string>(6);
-            strObjectHash.Add(0, "To");
-            strObjectHash.Add(1, "be");
-            strObjectHash.Add(2, "or");
-            strObjectHash.Add(3, "not");
-            strObjectHash.Add(4, "To");
-            strObjectHash.Add(5, "be");
-            string findValue = strObjectHash.getValue(4);
-            Console.WriteLine("value of an key is :"+findValue);
-
+            string goodThought = "To be or not to be";
+            string[] separator = goodThought.Split(" ");
+            HashMap<int, string> objHashh = new HashMap<int, string>(separator.Length);
+            int key = 0;
+            foreach (string word in separator)
+            {
+                objHashh.Add(key, word);
+                key++;
+            }
+            string findVal = objHashh.getValue(3);
+            Console.WriteLine(" Value of an incex is =" + findVal);
         }
     }
 }
